d649a5117005a5516491f6b8bd79b4c801e890e4
dleiva1905 <dleiva@fec.luz.edu.ve>
2017-07-12 09:19:02 -0400

    Update connect2db.php

--- serp/php/connect2db.php
+++ serp/php/connect2db.php
@@ -1,6 +1,6 @@
 <?php
 
-$mysqli = new MySQLi("localhost", "root","JP{G7jFp", "serp");
+$mysqli = new MySQLi("localhost", "root","{Lov/x}", "serp");
 $acentos = mysqli_query($mysqli,"SET NAMES 'utf8'");
 if ($mysqli -> connect_errno) {
 	die( "Fallo la conexión a MySQL: (" . $mysqli -> mysqli_connect_errno()
